b908add3616a4a9d3a31cb289e5bfbf4381970f1
Adam Ousmer <114683902+AdamOusmer@users.noreply.github.com>
2023-07-05 05:54:10 -0400

    Add menu and adjust cursor effect on it.
    TODO Add actions on it, checkbox already there

--- src/javascript/cursor.js
+++ src/javascript/cursor.js
@@ -1,5 +1,6 @@
 // Cursor
 
+// Tracker
 const cursor = document.querySelector('.cursor');
 
 // Tracker
@@ -7,6 +8,18 @@ document.addEventListener('mousemove', e => {
   cursor.setAttribute('style', `
   top: ${e.pageY}px; 
   left: ${e.pageX}px;
-  `);
+  `)});
 
-} );
+// Add event listeners to hoverable elements
+const hoverableElements = document.querySelectorAll('.hoverable');
+
+hoverableElements.forEach(element => {
+  element.addEventListener('mouseover', () => {
+    cursor.classList.add('hovered');
+  });
+  element.addEventListener('mouseout', () => {
+    cursor.classList.remove('hovered');
+  });
+
+});
+
