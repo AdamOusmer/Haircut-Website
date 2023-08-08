a8d2a7af55bcd90f9578462949277e7c28f72210
Adam Ousmer <114683902+AdamOusmer@users.noreply.github.com>
2023-08-08 14:20:08 +0200

    About section done
    Took 2 hours 14 minutes

--- src/javascript/general.js
+++ src/javascript/general.js
@@ -6,6 +6,7 @@ const cursorCircle = document.querySelector('.cursorCircle')
 let targetX = 0;
 let targetY = 0;
 let mouseHover = false; // Check if an element is hovered for the mouseup animation resizing
+let menuOpened = false;
 
 // Tracker
 document.addEventListener('mousemove', e => {
@@ -69,10 +70,12 @@ checkbox.addEventListener('change', () => {
         menu.classList.add('menuVisible');
         menu.classList.remove('menuInvisible')
         document.body.classList.add('menu-opened')
+        menuOpened = true;
     } else {
         menu.classList.add('menuInvisible')
         menu.classList.remove('menuVisible');
         document.body.classList.remove('menu-opened')
+        menuOpened = false;
     }
 });
 
@@ -85,5 +88,59 @@ function scrollWin() {
     });
 }
 
+// about button
+
+const contactButton = document.querySelector('.contactButton');
+const moreButton = document.querySelector('.moreButton');
+const contactStyle = document.querySelector('.contact')
+const moreStyle = document.querySelector('.more')
+const about = document.querySelector('.about')
+
+contactButton.addEventListener('mouseenter', () => {
+    contactStyle.classList.add('hoveredButtons');
+    moreButton.style.visibility = 'hidden';
+    about.style.background = '#0d0d0d';
+});
+
+contactButton.addEventListener('mouseleave', () => {
+    contactStyle.classList.remove('hoveredButtons');
+    moreButton.style.visibility = 'visible';
+    about.style.background = '#E6E6EB';
+
+});
 
 
+moreButton.addEventListener('mouseenter', () => {
+    moreStyle.classList.add('hoveredButtons');
+    contactButton.style.visibility = 'hidden';
+    about.style.background = '#0d0d0d';
+
+});
+
+moreButton.addEventListener('mouseleave', () => {
+    moreStyle.classList.remove('hoveredButtons');
+    contactButton.style.visibility = 'visible';
+    about.style.background = '#E6E6EB';
+
+});
+
+// menu Logo animation
+
+const menuLogo = Array.from(document.getElementsByClassName('.imageMenu'));
+
+function animateLogo() {
+
+    let maxExtension = [30, 25, 15, 10, 5];
+    if(menuOpened) {
+
+        menuLogo.forEach((logo, index) => {
+            let x = targetX / window.innerWidth;
+            let y = targetY / window.innerHeight;
+
+            logo.style.transform = 'translate(' + (x > maxExtension[index] ? maxExtension[index] : x) + '%, ' + (y > maxExtension[index] ? maxExtension[index] : y) + '%)';
+
+        })
+    }
+}
+
+window.addEventListener('mousemove', animateLogo);
