b950003b9448ec407afe0e67e287b9f0bbc1620b
Adam Ousmer <114683902+AdamOusmer@users.noreply.github.com>
2023-08-13 14:27:30 +0200

    Hairs services done
    Took 1 hour 5 minutes

--- src/javascript/hairs.js
+++ src/javascript/hairs.js
@@ -1,7 +1,21 @@
 // Animation when card switch
 
-const cards = document.getElementsByClassName('cardsServices');
-
+const cardsOrder = Array(document.querySelector('.first'), document.querySelector('.second'), document.querySelector('.third'), document.querySelector('.fourth'), document.querySelector('.fifth'));
+let activeCard = document.querySelector('.active');
 function changeCard(index){
+
+
+    if (cardsOrder[index] === activeCard) {
+        return;
+    }
+
+    // Animate the active card with a fade out
+
+    activeCard.classList.remove('active');
+
+    cardsOrder[index].classList.add('active');
+
+    activeCard = cardsOrder[index];
+
 
 }
