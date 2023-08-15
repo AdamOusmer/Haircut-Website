d31055301d59aae203e88df995c57e47a57e8fba
Adam Ousmer <114683902+AdamOusmer@users.noreply.github.com>
2023-08-15 16:07:52 +0200

    Cards Collections
    Took 6 hours 7 minutes

--- src/javascript/hairs.js
+++ src/javascript/hairs.js
@@ -2,7 +2,8 @@
 
 const cardsOrder = Array(document.querySelector('.first'), document.querySelector('.second'), document.querySelector('.third'), document.querySelector('.fourth'), document.querySelector('.fifth'));
 let activeCard = document.querySelector('.active');
-function changeCard(index){
+
+function changeCard(index) {
 
 
     if (cardsOrder[index] === activeCard) {
@@ -17,5 +18,51 @@ function changeCard(index){
 
     activeCard = cardsOrder[index];
 
-
 }
+
+const cards = document.querySelector('.cards');
+const nextButton = document.querySelector('.next');
+const previousButton = document.querySelector('.previous');
+const rotationValue = (2 * Math.PI) / 3;
+
+let rotationAngle = 0; // Initial rotation angle
+
+
+nextButton.addEventListener('click', () => {
+    rotationAngle -= rotationValue; // Decrease the rotation angle by 45 degrees
+
+    cards.animate(
+        [
+            { transform: `translate(-50%, 0%) rotate(${rotationAngle + rotationValue}rad)` },
+            { transform: `translate(-50%, 0%) rotate(${rotationAngle}rad)` }
+        ],
+        {
+            duration: 600, // Animation duration in milliseconds
+            easing: 'ease-in', // Animation easing function
+            fill: 'forwards'
+        }
+    );
+
+    cards.style.transform = `translate(-50%, 0%) rotate(${rotationAngle + rotationValue}rad)`
+
+});
+
+previousButton.addEventListener('click', () => {
+    rotationAngle += rotationValue; // Decrease the rotation angle by 45 degrees
+
+    cards.animate(
+        [
+            { transform: `translate(-50%, 0%) rotate(${rotationAngle - rotationValue}rad)` },
+            { transform: `translate(-50%, 0%) rotate(${rotationAngle}rad)` }
+        ],
+        {
+            duration: 600, // Animation duration in milliseconds
+            easing: 'ease-in', // Animation easing function
+            fill: 'forwards'
+        }
+    );
+
+    cards.style.transform = `translate(-50%, 0%) rotate(${rotationAngle - rotationValue}rad)`
+
+});
+
